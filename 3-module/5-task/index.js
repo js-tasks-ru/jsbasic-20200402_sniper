36df6dae56990f677701fec138b6084d945ac5f7
mnmlsniper <mnml.sniper@gmail.com>
2020-04-24 23:38:48 +0300

    3-5 task

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -4,5 +4,17 @@
  * @returns {{min:number, max:number}}  объект
  */
 function getMinMax(str) {
-  // ваш код...
+  // Очищаем строку и приводим к формату, когда все числа разделены запятой
+  str = str.replace(/[^0-9,\.\- ]/g, '')
+    .replace(/ /g, ',')
+    .replace(/,+/g, ',')
+    .replace(/^,|,$/g, '');
+  // Разбиваем строку на числа и приводим их к Float
+  const nums = str.split(',').map(Number.parseFloat);
+
+  // Находим min/max и возвращаем нужную структуру
+  return {
+    min: Math.min(...nums),
+    max: Math.max(...nums)
+  };
 }
