42c821fb7510ccbb07217ccc08a683c40c0c507d
mnmlsniper <mnml.sniper@gmail.com>
2020-04-27 17:38:29 +0300

    add 4-1 task

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -4,5 +4,12 @@
  * @return {HTMLUListElement}
  */
 function makeFriendsList(friends) {
-  // ваш код...
+  let ul = document.createElement('ul');
+  let li = {};
+  for (friend of friends) {
+    li = document.createElement('li');
+    li.innerHTML = `${friend.firstName} ${friend.lastName}`;
+    ul.appendChild(li);
+  }
+  return ul;
 }
